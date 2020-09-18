9bf761cdc633cbc9ab4b0e0d2ac751089083e364
CyberSanya2008 <derzhaev59@gmail.com>
2020-09-18 20:37:57 +0700

    Send data

--- functions.js
+++ functions.js
@@ -1,8 +1,32 @@
-function GetUserRegistrationData(){
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-    console.log(name)
-    console.log(email)
-    console.log(password)
+const request = require("request");
+
+// Получение данных пользователя из формы с регистрацией и отправка на сервер
+function GetUserRegistrationData() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  
+
+  // Отправка данных на сервер
+  request.post(
+    "http://127.0.0.1:5000/registration",
+    {
+      json: {
+        name: name,
+        email: email,
+        password: password,
+      },
+    },
+    (error, res, body) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+      console.log(`statusCode: ${res.statusCode}`);
+      console.log(body);
+    }
+  );
+
+  
 }
