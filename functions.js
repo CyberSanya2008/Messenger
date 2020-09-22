502177a163449879362b7ce7929d1aea42e9eb1e
CyberSanya2008 <derzhaev59@gmail.com>
2020-09-22 21:04:36 +0700

    First iteration of show messages and dilogs functions

--- functions.js
+++ functions.js
@@ -84,7 +84,7 @@ function SendMessage() {
   let user = JSON.parse(rawdata);
 
   let reciever = document.getElementById("reciever").value;
-  let message_text = document.getElementById('message_text').value
+  let message_text = document.getElementById("message_text").value;
 
   request.post(
     "http://127.0.0.1:5000/sendmessage",
@@ -104,5 +104,78 @@ function SendMessage() {
       console.log(body);
     }
   );
+}
 
+function ShowMessages() {
+  let rawdata = fs.readFileSync("current_user.json");
+  let user = JSON.parse(rawdata);
+
+  const options = {
+    method: "GET",
+    uri:
+      "http://127.0.0.1:5000/profile/" +
+      user.email +
+      "/" +
+      user.password +
+      "/messages",
+  };
+  requests(options)
+    .then(function (response) {
+      // Запрос на сервер
+      if (response == "error") {
+        console.log("error");
+      } else {
+        // Запись данных пользователя в файл после авторизации
+        // в случае успешной авторизации
+        let current_user = {
+          email: email,
+          name: response,
+          password: password,
+        };
+
+        let data = JSON.stringify(current_user);
+        fs.writeFileSync("current_user.json", data);
+
+        // Переход на страницу пользователя
+        window.location.href = "profile.html";
+      }
+    })
+    .catch(function (err) {
+      // Произошло что-то плохое, обработка ошибки
+    });
+}
+
+function ShowDialogs() {
+  let rawdata = fs.readFileSync("current_user.json");
+  let user = JSON.parse(rawdata);
+
+  const options = {
+    method: "GET",
+    uri:
+      "http://127.0.0.1:5000/profile/dialogs/" +
+      user.email +
+      "/" +
+      user.password,
+  };
+  requests(options)
+    .then(function (response) {
+      // Запрос на сервер
+      if (response == "error") {
+        console.log("error");
+      } else {
+        // Запись данных пользователя в файл после авторизации
+        // в случае успешной авторизации
+
+        let data = JSON.parse(response)
+
+        let username = document.getElementById("leftbox");
+        username.innerHTML = "<h1>" + data[1];
+        
+
+        console.log(JSON.parse(response));
+      }
+    })
+    .catch(function (err) {
+      // Произошло что-то плохое, обработка ошибки
+    });
 }
